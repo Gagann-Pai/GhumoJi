7251827027716c5e2aa4e380cd84dc81b3587bd9
Gagann-Pai <gaganpai0777@gmail.com>
2026-07-16 20:25:41 +0530

    updated frontend2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,21 +8,28 @@ function SearchBar({ onSearch }) {
     const [error, setError] = useState("");
 
     async function searchPlaces() {
-  try {
-    const response = await axios.get(`https://tourism-backend-x2h9.onrender.com/api/places/search?name=${placeName.trim()}`);
-    onSearch(response.data);
-  }
-  catch (err) {
-    console.log(err);
-    setError("Place not found");
-  }
-}
+        try {
+            const response = await axios.get(`https://tourism-backend-x2h9.onrender.com/api/places/search?name=${placeName.trim()}`);
+            
+            if (response.data.length === 0) {
+                setError("😕 No places found.Try searching for another destination.");
+                onSearch([]); 
+            } else {
+                setError(""); 
+                onSearch(response.data);
+            }
+            
+        } catch (err) {
+            console.log(err);
+            setError("Failed to connect to the server");
+        }
+    }
 
     return (
         <div className="searchBox">
             <input 
                 type="text" 
-                placeholder="type the name of place" 
+                placeholder="Search places like Taj Mahal, Hampi, Munnar..." 
                 value={placeName} 
                 onChange={(e) => setPlaceName(e.target.value)}
             />
